48300cad85232df73f5567fc850d51c58b0d7a90
Maria Gavrilova <mar.gavrilova@gmail.com>
2025-09-10 00:39:48 +0300

    Добавляет код для конвертации валют через функции

--- main.go
+++ main.go
@@ -8,3 +8,22 @@ func main() {
 	ERub := URub / UEur
 	fmt.Print(ERub)
 }
+
+func currencyInput(float64, string, string) {
+	var moneyAmount float64
+	var currentCurrency string
+	var targetCurrency string
+	fmt.Print("Введите количество денег, которое мы хотим сконвертировать:")
+	fmt.Scan(&moneyAmount)
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&currentCurrency)
+	fmt.Print("Введите таргетированную валюту: ")
+	fmt.Scan(&targetCurrency)
+	return moneyAmount, currentCurrency, targetCurrency
+}
+
+func currencyCalculate(float64, string, string) {
+	moneyAmount, currentCurrency, targetCurrency := currencyInput()
+
+	return result
+}
